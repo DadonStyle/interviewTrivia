7ad441d2aafd18a4d6adf86af55bc56d18610fff
Sage Baram <sagebaram.career@gamil.com>
2024-01-03 00:48:43 +0200

    (ref) change to mui components, add inline style

--- src/components/ui/OptionList.tsx
+++ src/components/ui/OptionList.tsx
@@ -1,24 +1,50 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+
 import { shuffle } from "lodash";
-import { v4 as uuid } from "uuid";
 import { Question } from "../../constants/types";
+
+interface OptionListProps {
+  question: Question
+}
 
 const prepSelection = (question: Question) => {
   const allOptions = [question.answer, ...question.wrongAnswers];
   return shuffle(allOptions);
 };
 
-interface OptionListProps {
-  question: Question
-}
+const indexToAlpha = (index: number) => {
+  return String.fromCharCode(65 + index);
+};
 
 const OptionList = ({ question }: OptionListProps) => {
   const options = prepSelection(question);
   return (
-    <ol type="A">
-      {options.map((option: string) => (
-        <li key={uuid()}><button>{option}</button></li>
-      ))}
-    </ol>
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <List>
+        {options.map((option: string, index: number) => (
+          <ListItem key={crypto.randomUUID()}>
+            <Button
+              variant="contained"
+              sx={{
+                padding: "0.625rem",
+                backgroundColor: "white",
+                color: "black"
+              }}
+            >
+              {`${indexToAlpha(index)}. ${option}`}
+            </Button>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 };
 
