70fd8ff2dd5f8c11c603b84dc6156fb283cef782
Sage Baram <sagebaram.career@gamil.com>
2024-01-03 02:29:37 +0200

    (ref) use absolute imports, format

--- src/components/ui/Categories.tsx
+++ src/components/ui/Categories.tsx
@@ -1,16 +1,27 @@
-import { Button, Box } from "@mui/material";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Background from "../Background/Background";
 
 const categories = ["React", "Typescript", "Javascript", "Random"];
 
 const Categories = () => {
   return (
-    <Box>
-      {categories.map((category) => {
-        return (
-          <Button variant="contained" onClick={() => console.log("Hello")} type="button" key={category}>{category}</Button>
-        );
-      })}
-    </Box>
+    <Background>
+      <Box>
+        {categories.map((category) => {
+          return (
+            <Button
+              variant="contained"
+              onClick={() => console.log("Hello")}
+              type="button"
+              key={category}
+            >
+              {category}
+            </Button>
+          );
+        })}
+      </Box>
+    </Background>
   );
 };
 
