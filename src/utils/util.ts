59f1e1584999e8f53fcd9be91d8ecc20771bf720
Sage Baram <sagebaram.career@gamil.com>
2024-01-05 03:17:50 +0200

    (feat) add utility functions

---
+++ src/utils/util.ts
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+import allQuestions from "../constants/questions";
+import { QuestionType } from "../constants/types";
+
+export const randomVal = (maxVal: number) => Math.floor((Math.random() * maxVal));
+export const randomIndex = (array: any) => randomVal(array.length);
+export const indexToAlpha = (index: number) => String.fromCharCode(65 + index);
+export const shuffle = (array: any) => array.sort(() => Math.random() - 0.5);
+export const getQuestions = () => {
+  const { category } = useParams<string>();
+  switch (category) {
+    case "react":
+      return allQuestions.react;
+    case "typescript":
+      return allQuestions.typescript;
+    case "javscript":
+      return allQuestions.javascript;
+    default:
+      return [];
+  }
+};
+
+export const prepSelection = (question: QuestionType) => {
+  const allOptions = [question.answer, ...question.wrongAnswers];
+  return shuffle(allOptions);
+};
