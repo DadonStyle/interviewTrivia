5d0db725658feab0d2d0f6f496fd7cffbffc5808
Sage Baram <sagebaram.career@gamil.com>
2024-01-05 03:12:25 +0200

    (ref) split json to 3 jsons

---
+++ src/constants/questions.ts
@@ -0,0 +1,9 @@
+import reactQuestions from "./react.json";
+import typescriptQuestions from "./typescript.json";
+import javascriptQuestions from "./javascript.json";
+
+export default {
+  react: reactQuestions,
+  typescript: typescriptQuestions,
+  javascript: javascriptQuestions,
+};
