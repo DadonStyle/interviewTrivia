4b1a892e6d92846165638804c801773dc3816f62
Sage Baram <sagebaram.career@gamil.com>
2024-01-07 14:06:35 +0200

    (ref) ref logic, remove cache

--- src/modules/questionary/Questionary.tsx
+++ src/modules/questionary/Questionary.tsx
@@ -1,42 +1,46 @@
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useScore } from "../../contexts/ScoreContext";
-import { getQuestions, randomIndex } from "../../utils/util";
+import { getShuffledQuestions } from "../../utils/util";
 
 import Question from "../question/Question";
 import "./Questionary.scss";
 import { QuestionType } from "../../constants/types";
 
-const Questionary = () => {
-  const questions = getQuestions();
-  const index = randomIndex(questions);
+const QuestionaryContainer = () => {
+  const questions = getShuffledQuestions();
+  return <Questionary questions={questions} />;
+};
+
+interface QuestionaryProps {
+  questions: QuestionType[];
+}
+
+const Questionary = ({ questions }: QuestionaryProps) => {
+  const [questionIndex, setQuestionIndex] = useState(0);
   const navigate = useNavigate();
   const { score, setScore } = useScore();
-  const [cache, setCache] = useState<number[]>([index]);
-  const [randomQuestion, setRandomQuestion] = useState<QuestionType>(
-    questions[index]
-  );
+  const [selectedOption, setSelectedOption] = useState<string>("");
 
   const onOptionSelect = (selectedOption: string) => {
-    if (cache.length >= questions.length) {
-      navigate("/review");
-    }
+    setSelectedOption(selectedOption);
+  };
 
-    let index;
-    do {
-      index = randomIndex(questions);
-    } while (cache.includes(index) && cache.length < questions.length);
+  const handleSubmit = () => {
+    if (!selectedOption) return;
+    if (questionIndex > questions.length) navigate("/review");
 
-    if (randomQuestion.answer === selectedOption) {
+    if (questions[questionIndex].answer === selectedOption) {
       setScore((prevScore) => prevScore + 1);
     } else {
       setScore((prevScore) => prevScore - 1);
     }
 
-    setCache([...cache, index]);
-    setRandomQuestion(questions[index]);
+    setSelectedOption("");
+    setQuestionIndex((prevIndex) => prevIndex + 1);
   };
 
   return (
@@ -47,8 +51,9 @@ const Questionary = () => {
       <Typography paragraph>
         Score: {score}
       </Typography>
-      <Question question={randomQuestion} onOptionSelect={onOptionSelect} />
+      <Question question={questions[questionIndex]} onOptionSelect={onOptionSelect} />
+      <Button type="submit" onClick={handleSubmit}>Next</Button>
     </Box>);
 };
 
-export default Questionary;
+export default QuestionaryContainer;
